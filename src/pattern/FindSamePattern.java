a4b4e6466045f13f3c9f2885d93c1f188e51d275
이영지 <youngjee@lgcns.com>
2018-07-04 17:55:06 +0900

    같은패턴을 찾는 문제

---
+++ src/pattern/FindSamePattern.java
@@ -0,0 +1,31 @@
+package pattern;
+
+public class FindSamePattern {
+	public static void main(String[] args) {
+		String[] strArr = {"config.sys", "config.inf", "config.ure"};
+		System.out.println(findSameStr(strArr));
+		
+	}
+	
+	public static String findSameStr(String[] arr){
+		String compared = arr[0];
+		
+		for (int i = 1; i < arr.length; i++) {
+			StringBuffer str = new StringBuffer();
+			char[] compArr = compared.toCharArray();
+			char[] charArr = arr[i].toCharArray();
+			
+			for (int j = 0; j < compArr.length; j++) {
+				if(j<charArr.length){
+					if(compArr[j]==charArr[j]){
+						str.append(compArr[j]);
+					}else{
+						str.append("?");
+					}
+				}
+			}
+			compared = str.toString();
+		}
+		return compared;
+	}
+}
